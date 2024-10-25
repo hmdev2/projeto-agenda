f8e6277bce0d1f6116767da7d5635c3d92a7050f
hmdev2 <hmdev2@proton.me>
2024-10-25 20:16:09 -0300

    back e front

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController.js');
 const loginController = require('./src/controllers/loginController.js');
+const contatoController = require('./src/controllers/contatoController.js');
+
+const { loginRequired } = require('./src/middlewares/middleware.js');
 
 //Rotas da home
 route.get('/', homeController.index);
@@ -12,5 +15,12 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+//Rotas criação de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
 
 module.exports = route;
